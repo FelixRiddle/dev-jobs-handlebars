16a1f376fb31449b403e04095a3350264b43da0c
Felix <fedecraft3312@gmail.com>
2024-06-20 22:48:53 -0300

    Ironic, I couldn't make it work with handlebars but did with nextjs

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const Job = require("../model/Job");
 
 const homeRouter = express.Router();
 
-homeRouter.get("/", (req, res) => {
+homeRouter.get("/", async (req, res, next) => {
+	const jobs = await Job.find();
+	
+	if(!jobs) {
+		return next();
+	}
+	
     return res.render('home', {
         title: "Homepage",
         tagline: "Find and post developer jobs",
         bar: true,
         button: true,
+		jobs,
     });
 });
 
--- src/routes/job/index.js
+++ src/routes/job/index.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const Job = require('../../model/Job');
 
 const createRouter = require("./create");
 
 const jobRouter = express.Router();
 
 jobRouter.use(createRouter);
+
+jobRouter.get("/get_all", async (req, res) => {
+	try {
+		const jobs = await Job.find();
+		
+		return res.send({
+            jobs,
+        });
+	} catch(err) {
+		console.error(err);
+		return res.status(500).send({
+			messages: [{
+                message: "Internal error",
+                error: true,
+            }],
+		});
+	}
+});
 
 module.exports = jobRouter;
