09ef5b4ad600d2289de05d8dbef814c36373f79f
Felix <fedecraft3312@gmail.com>
2024-06-25 16:41:02 -0300

    Edit

--- public/js/job/edit.js
+++ public/js/job/edit.js
@@ -1,3 +1,49 @@
+// I really need to use webpack :P
+const skills = new Set();
+
+/**
+ * Add skill
+ */
+function addSkill(e) {
+	if(e.target.tagName === "LI") {
+		if(e.target.classList.contains("activo")) {
+			skills.delete(e.target.textContent);
+			e.target.classList.remove("activo");
+		} else {
+			skills.add(e.target.textContent);
+			e.target.classList.add("activo");
+		}
+	}
+	
+	const skillsArray = [...skills];
+	document.querySelector("#skills").value = skillsArray;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const skillElements = document.querySelector(".lista-conocimientos");
+	
+	if(skillElements) {
+		skillElements.addEventListener("click", addSkill);
+		
+		// Once we are editing, call the function
+		selectSkills();
+	}
+});
+
+/**
+ * Select skills
+ */
+function selectSkills() {
+	const selected = Array.from(document.querySelectorAll(".lista-conocimientos .activo"));
+	
+	const selectedSkills = selected.forEach(skill => skill.textContent);
+	
+	selectedSkills.forEach(select => {
+		skills.add(select);
+	});
+	
+	document.querySelector("#skills").value = selectedSkills;
+}
 
 /**
  * Select contract
