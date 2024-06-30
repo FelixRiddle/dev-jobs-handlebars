13709bb5d76ba84cfbfedbca6b4563f1bbac662c
Felix <fedecraft3312@gmail.com>
2024-06-29 22:57:28 -0300

    Create magic link to reset password

--- src/routes/auth/resetPassword.js
+++ src/routes/auth/resetPassword.js
@@ -1,14 +1,61 @@
 const express = require('express');
 const expandData = require('../../lib/misc/expand');
+const User = require('../../model/User');
+const crypto = require("crypto");
 
 const resetPasswordRouter = express.Router();
 
+const metadata = {
+	title: "Reset password",
+	tagline: "Reset your password using a link sent to your email",
+};
+
 resetPasswordRouter.get("/", (req, res) => {
 	return res.render("auth/reset-password", {
-		title: "Reset password",
-		tagline: "Reset your password using a link sent to your email",
+		...metadata,
 		...expandData(req),
 	});
+});
+
+resetPasswordRouter.post("/", async (req, res) => {
+	try {
+		console.log(`[POST] /auth/reset-password`);
+		
+		const user = await User.findOne({
+			email: req.body.email,
+		});
+		
+		if(!user) {
+			req.flash('messages', [{
+				message: "No user found with that email",
+                error: true,
+			}]);
+			return res.render("auth/reset-password", {
+				...metadata,
+				...expandData(req),
+			});
+		}
+		
+		user.token = crypto.randomBytes(20).toString("hex");
+		user.expires = Date.now() + 3_600_000;
+		
+		await user.save();
+		
+		// TODO: 
+		const magicLink = `http://${req.headers.host}/reset-password/token/${user.token}`;
+		
+		req.flash('messages', [{
+			message: "We've sent you an E-Mail with instructions",
+			error: false,
+		}]);
+		
+		return res.redirect(
+			'/auth/login'
+		);
+	} catch(err) {
+		console.error(err);
+		return res.redirect("/500");
+	}
 });
 
 module.exports = resetPasswordRouter;
